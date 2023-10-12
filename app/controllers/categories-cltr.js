14a7f233363c8352a3cfd82918041fbb3a928110
aniruddha <ani@dctacademy.com>
2023-10-12 14:02:44 +0530

    custom validation in shcema

--- app/controllers/categories-cltr.js
+++ app/controllers/categories-cltr.js
@@ -17,17 +17,12 @@ categoriesCltr.create = async (req, res) => {
         return res.status(400).json({ errors: errors.array() })
     }
     const body = req.body 
-    const categoryObj = await Category.findOne({ name: { '$regex' : body.name, $options: 'i' } })
-    if(!categoryObj) {
-        const category = new Category(body) 
-        try {
-            await category.save()
-            res.json(category)
-        } catch(e) {
-            res.status(500).json(e)
-        }
-    } else {
-        res.status(204).json({ error: 'category already present'})
+    const category = new Category(body) 
+    try {
+        await category.save()
+        res.json(category)
+    } catch(e) {
+        res.status(500).json(e)
     }
 }
 
--- app/helpers/category-validation.js
+++ app/helpers/category-validation.js
@@ -1,7 +1,18 @@
+const Category = require('../models/category-model')
 const categoryValidationSchema = {
     name: {
         notEmpty: {
             errorMessage: 'category name is required'
+        },
+        custom: {
+            options: async (value) => {
+                const category = await Category.findOne({ name: { '$regex' : value, $options: 'i'}})
+                if(!category) {
+                    return true 
+                } else {
+                    throw new Error('category already present')
+                }
+            }
         }
     }
 }
